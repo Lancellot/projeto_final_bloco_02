667a59cb1893f4adc3683c71baccd576e147efbe
Lancellot <assis.neto@rede.ulbra.br>
2025-12-17 12:30:09 -0300

    feat: implementação do swagger par documentar

--- src/categoria/entities/categoria.entity.ts
+++ src/categoria/entities/categoria.entity.ts
@@ -1,16 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({name: 'tb_categoria'})
 export class Categoria {
 
+    @ApiProperty()
     @PrimaryGeneratedColumn()
     id: number;
 
+    @ApiProperty()
     @IsNotEmpty()
     @Column({ length:255, nullable:false })
     nome: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     @Column({length:500, nullable:true })
     descricao: string;
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Farmacia')
+    .setDescription('Projeto Final Bloco 2 - Generation Brasil')
+    .setVersion('0.1.0')
+    .addTag('Farmacia')
+    .build();
+    const document =  SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/swagger', app, document);
 
   process.env.TZ = '-03:00';
 
