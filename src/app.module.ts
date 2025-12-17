0a8abcee09a652d9aca2e0d8a41526b218df100f
Lancellot <assis.neto@rede.ulbra.br>
2025-12-17 09:43:39 -0300

    feat: configuração inicial do projeto e banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriaModule } from './categoria/categoria.module';
 
 
 @Module({
@@ -12,7 +13,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       password: process.env.DB_PASSWORD,
       entities: [],
       synchronize: true,
-    })
+    }),
+    CategoriaModule,
   ],
   controllers: [],
   providers: [],
